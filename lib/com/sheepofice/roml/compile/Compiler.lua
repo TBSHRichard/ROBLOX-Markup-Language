4366a4a6fcec1078fa18c26e589be175c2fb4f47
Richard Voelker <tbsh_richard@blacksheepherd.net>
2015-07-29 19:37:43 -0400

    Refactored parts of the Compiler to add support for adding multiple lines to an update function (if the same variable appears multiple times throughout the RoML document).

--- lib/com/sheepofice/roml/compile/Compiler.lua
+++ lib/com/sheepofice/roml/compile/Compiler.lua
@@ -7,38 +7,41 @@ local VariableNamer = require("com.sheepofice.roml.compile.VariableNamer")
 local CompilerPropertyFilter = require("com.sheepofice.roml.compile.CompilerPropertyFilter")
 local addCode = nil
 local addCodeFunctions = nil
+local writeVarCode
+writeVarCode = function(mainBlock, functionTable, varNamer, className, varName, changeFunction)
+  if not (functionTable[varName]) then
+    functionTable[varName] = FunctionBlock("varChange_" .. tostring(varName), "")
+  end
+  local objectName = varNamer:NameObjectVariable(className)
+  mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local " .. tostring(objectName)))
+  mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local varChange_" .. tostring(varName)))
+  local varChange = functionTable[varName]
+  changeFunction(varChange, objectName, varName)
+  mainBlock:AddChild(MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange)
+  mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(varName) .. " = RomlVar(vars." .. tostring(varName) .. ")"))
+  mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(varName) .. ".Changed:connect(varChange_" .. tostring(varName) .. ")"))
+  mainBlock:AddChild(MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_" .. tostring(varName) .. "()"))
+  return objectName
+end
 local writeObjectToBlock
-writeObjectToBlock = function(mainBlock, builderParam, className, id, classes, properties, children)
+writeObjectToBlock = function(mainBlock, functionTable, varNamer, builderParam, className, id, classes, properties, children)
   local classesString = "nil"
-  local varNamer = VariableNamer()
   local objectName = nil
   if classes and classes[1] == "static" then
     classesString = Table.ArrayToSingleLineString(classes[2])
   elseif classes and classes[1] == "dynamic" then
-    objectName = varNamer:NameObjectVariable(className)
-    mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local " .. tostring(objectName)))
-    mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local varChange_" .. tostring(classes[2])))
-    local varChange = FunctionBlock("varChange_" .. tostring(classes[2]), "")
-    varChange:AddChild(Line(tostring(objectName) .. ":SetClasses(self._vars." .. tostring(classes[2]) .. ":GetValue())"))
-    mainBlock:AddChild(MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange)
-    mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(classes[2]) .. " = RomlVar(vars." .. tostring(classes[2]) .. ")"))
-    mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(classes[2]) .. ".Changed:connect(varChange_" .. tostring(classes[2]) .. ")"))
-    mainBlock:AddChild(MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_" .. tostring(classes[2]) .. "()"))
+    objectName = writeVarCode(mainBlock, functionTable, varNamer, className, classes[2], function(varChange, objectName, varName)
+      return varChange:AddChild(Line(tostring(objectName) .. ":SetClasses(self._vars." .. tostring(varName) .. ":GetValue())"))
+    end)
   end
   if properties then
     for name, value in properties:pairs() do
       if type(value) == "string" then
         properties[name] = CompilerPropertyFilter.FilterProperty(className, name, value)
       else
-        objectName = varNamer:NameObjectVariable(className)
-        mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local " .. tostring(objectName)))
-        mainBlock:AddChild(MainBlock.BLOCK_VARS, Line("local varChange_" .. tostring(value[2])))
-        local varChange = FunctionBlock("varChange_" .. tostring(value[2]), "")
-        varChange:AddChild(Line(tostring(objectName) .. ":SetProperties({" .. tostring(name) .. " = self._vars." .. tostring(value[2]) .. ":GetValue()})"))
-        mainBlock:AddChild(MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange)
-        mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(value[2]) .. " = RomlVar(vars." .. tostring(value[2]) .. ")"))
-        mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("self._vars." .. tostring(value[2]) .. ".Changed:connect(varChange_" .. tostring(value[2]) .. ")"))
-        mainBlock:AddChild(MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_" .. tostring(value[2]) .. "()"))
+        objectName = writeVarCode(mainBlock, functionTable, varNamer, className, value[2], function(varChange, objectName, varName)
+          return varChange:AddChild(Line(tostring(objectName) .. ":SetProperties({" .. tostring(name) .. " = self._vars." .. tostring(varName) .. ":GetValue()})"))
+        end)
         properties[name] = nil
       end
     end
@@ -61,19 +64,21 @@ writeObjectToBlock = function(mainBlock, builderParam, className, id, classes, p
   return mainBlock:AddChild(MainBlock.BLOCK_CREATION, Line("builder:Pop()"))
 end
 addCodeFunctions = {
-  object = function(mainBlock, obj)
+  object = function(mainBlock, functionTable, varNamer, obj)
     local _, className, id, classes, properties, children = unpack(obj)
-    return writeObjectToBlock(mainBlock, "\"" .. tostring(className) .. "\"", className, id, classes, properties, children)
+    return writeObjectToBlock(mainBlock, functionTable, varNamer, "\"" .. tostring(className) .. "\"", className, id, classes, properties, children)
   end,
-  clone = function(mainBlock, obj)
+  clone = function(mainBlock, functionTable, varNamer, obj)
     local _, className, robloxObject, id, classes, properties, children = unpack(obj)
-    return writeObjectToBlock(mainBlock, robloxObject, className, id, classes, properties, children)
+    return writeObjectToBlock(mainBlock, functionTable, varNamer, robloxObject, className, id, classes, properties, children)
   end
 }
 addCode = function(mainBlock, tree)
+  local functionTable = { }
+  local varNamer = VariableNamer()
   if tree then
     for _, obj in ipairs(tree) do
-      addCodeFunctions[obj[1]](mainBlock, obj)
+      addCodeFunctions[obj[1]](mainBlock, functionTable, varNamer, obj)
     end
   end
 end
--- src/com/sheepofice/roml/compile/Compiler.moon
+++ src/com/sheepofice/roml/compile/Compiler.moon
@@ -17,38 +17,38 @@ CompilerPropertyFilter = require "com.sheepofice.roml.compile.CompilerPropertyFi
 addCode = nil
 addCodeFunctions = nil
 
-writeObjectToBlock = (mainBlock, builderParam, className, id, classes, properties, children) ->
+writeVarCode = (mainBlock, functionTable, varNamer, className, varName, changeFunction) ->
+	functionTable[varName] = FunctionBlock "varChange_#{varName}", "" unless functionTable[varName]
+
+	objectName = varNamer\NameObjectVariable className
+	mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local #{objectName}")
+	mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local varChange_#{varName}")
+	varChange = functionTable[varName]
+	changeFunction varChange, objectName, varName
+	mainBlock\AddChild MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange
+	mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{varName} = RomlVar(vars.#{varName})")
+	mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{varName}.Changed:connect(varChange_#{varName})")
+	mainBlock\AddChild MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_#{varName}()")
+	return objectName
+
+
+writeObjectToBlock = (mainBlock, functionTable, varNamer, builderParam, className, id, classes, properties, children) ->
 	classesString = "nil"
-	varNamer = VariableNamer!
 	objectName = nil
 
 	if classes and classes[1] == "static"
 		classesString = Table.ArrayToSingleLineString(classes[2])
 	elseif classes and classes[1] == "dynamic"
-		objectName = varNamer\NameObjectVariable className
-		mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local #{objectName}")
-		mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local varChange_#{classes[2]}")
-		varChange = FunctionBlock "varChange_#{classes[2]}", ""
-		varChange\AddChild Line("#{objectName}:SetClasses(self._vars.#{classes[2]}:GetValue())")
-		mainBlock\AddChild MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange
-		mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{classes[2]} = RomlVar(vars.#{classes[2]})")
-		mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{classes[2]}.Changed:connect(varChange_#{classes[2]})")
-		mainBlock\AddChild MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_#{classes[2]}()")
+		objectName = writeVarCode mainBlock, functionTable, varNamer, className, classes[2], (varChange, objectName, varName) ->
+			varChange\AddChild Line("#{objectName}:SetClasses(self._vars.#{varName}:GetValue())")
 
 	if properties
 		for name, value in properties\pairs!
 			if type(value) == "string"
 				properties[name] = CompilerPropertyFilter.FilterProperty className, name, value
 			else
-				objectName = varNamer\NameObjectVariable className
-				mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local #{objectName}")
-				mainBlock\AddChild MainBlock.BLOCK_VARS, Line("local varChange_#{value[2]}")
-				varChange = FunctionBlock "varChange_#{value[2]}", ""
-				varChange\AddChild Line("#{objectName}:SetProperties({#{name} = self._vars.#{value[2]}:GetValue()})")
-				mainBlock\AddChild MainBlock.BLOCK_UPDATE_FUNCTIONS, varChange
-				mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{value[2]} = RomlVar(vars.#{value[2]})")
-				mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("self._vars.#{value[2]}.Changed:connect(varChange_#{value[2]})")
-				mainBlock\AddChild MainBlock.BLOCK_FUNCTION_CALLS, Line("varChange_#{value[2]}()")
+				objectName = writeVarCode mainBlock, functionTable, varNamer, className, value[2], (varChange, objectName, varName) ->
+					varChange\AddChild Line("#{objectName}:SetProperties({#{name} = self._vars.#{varName}:GetValue()})")
 				properties[name] = nil
 
 	objectName = "objTemp" if (id or properties) and not objectName
@@ -65,7 +65,7 @@ writeObjectToBlock = (mainBlock, builderParam, className, id, classes, propertie
 	mainBlock\AddChild MainBlock.BLOCK_CREATION, Line("builder:Pop()")
 
 addCodeFunctions =
-	object: (mainBlock, obj) ->
+	object: (mainBlock, functionTable, varNamer, obj) ->
 		-- {
 		--	"object"
 		--	ClassName							:string
@@ -75,9 +75,9 @@ addCodeFunctions =
 		--	Children							:array
 		-- }
 		_, className, id, classes, properties, children = unpack obj
-		writeObjectToBlock mainBlock, "\"#{className}\"", className, id, classes, properties, children
+		writeObjectToBlock mainBlock, functionTable, varNamer, "\"#{className}\"", className, id, classes, properties, children
 
-	clone: (mainBlock, obj) ->
+	clone: (mainBlock, functionTable, varNamer, obj) ->
 		-- {
 		--	"clone"
 		--	ClassName							:string
@@ -88,12 +88,15 @@ addCodeFunctions =
 		--	Children							:array
 		-- }
 		_, className, robloxObject, id, classes, properties, children = unpack obj
-		writeObjectToBlock mainBlock, robloxObject, className, id, classes, properties, children
+		writeObjectToBlock mainBlock, functionTable, varNamer, robloxObject, className, id, classes, properties, children
 
 addCode = (mainBlock, tree) ->
+	functionTable = {}
+	varNamer = VariableNamer!
+
 	if tree
 		for _, obj in ipairs tree
-			addCodeFunctions[obj[1]] mainBlock, obj
+			addCodeFunctions[obj[1]] mainBlock, functionTable, varNamer, obj
 
 ----------------------------------------------------------------
 -- Compile the parsetree into a Lua string.
